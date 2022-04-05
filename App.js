bc3818af369b7d3234179df07aed93271865829a
AhmedAdel19 <adel.eng20@gmail.com>
2022-04-05 14:04:27 +0200

    finalPush

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import {  StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import StackNavigation from './src/screens/StackNavigation';
+import MSSQL from 'react-native-mssql';
 
+ const App = () => {
 
-export default function App() {
+  async function openDB() {
+    const config = {
+      server: '192.168.1.19', //ip address of the mssql database
+      username: 'admin', //username to login to the database
+      password: '00000', //password to login to the database
+      database: 'AuragesDb01', //the name of the database to connect to
+      port: 1433, //OPTIONAL, port of the database on the server
+      // timeout: 5, //OPTIONAL, login timeout for the server
+    }
+    let cnx = ''
+    try {
+      cnx = await MSSQL.connect(config);
+      return true // 'Conectar ao Servidor: '+cnx
+    } catch (err) {
+      console.log("conn error : "+err)
+      return false// 'Conectar ao Servidor: Failled!'
+    }
+  }
+  openDB().then(async () => {
+    try {
+      const query = `select Guid, Number, Code as Name from TablesPlaces where HostGuid = NullGUID`
+
+      const result = await MSSQL.executeQuery(query);
+      alert(JSON.stringify(result))
+      console.log(result)
+      console.log('show readAll')
+      await MSSQL.close()
+    } catch (erro) {
+      console.log(erro)
+      console.log("Errorrr  :  "+erro)
+      await MSSQL.close()
+    }
+  })
   return (
     // <View style={styles.container}>
-      <StackNavigation />
+    <StackNavigation />
     // </View>
   );
 }
@@ -20,3 +54,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+export default App;
