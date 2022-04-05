c17773f2364169d1cf65312f9810d5ef458310bd
AhmedAdel19 <adel.eng20@gmail.com>
2022-04-05 14:12:05 +0200

    pushLast

--- App.js
+++ App.js
@@ -6,39 +6,39 @@ import MSSQL from 'react-native-mssql';
 
  const App = () => {
 
-  async function openDB() {
-    const config = {
-      server: '192.168.1.19', //ip address of the mssql database
-      username: 'admin', //username to login to the database
-      password: '00000', //password to login to the database
-      database: 'AuragesDb01', //the name of the database to connect to
-      port: 1433, //OPTIONAL, port of the database on the server
-      // timeout: 5, //OPTIONAL, login timeout for the server
-    }
-    let cnx = ''
-    try {
-      cnx = await MSSQL.connect(config);
-      return true // 'Conectar ao Servidor: '+cnx
-    } catch (err) {
-      console.log("conn error : "+err)
-      return false// 'Conectar ao Servidor: Failled!'
-    }
-  }
-  openDB().then(async () => {
-    try {
-      const query = `select Guid, Number, Code as Name from TablesPlaces where HostGuid = NullGUID`
+  // async function openDB() {
+  //   const config = {
+  //     server: '192.168.1.19', //ip address of the mssql database
+  //     username: 'admin', //username to login to the database
+  //     password: '00000', //password to login to the database
+  //     database: 'AuragesDb01', //the name of the database to connect to
+  //     port: 1433, //OPTIONAL, port of the database on the server
+  //     // timeout: 5, //OPTIONAL, login timeout for the server
+  //   }
+  //   let cnx = ''
+  //   try {
+  //     cnx = await MSSQL.connect(config);
+  //     return true // 'Conectar ao Servidor: '+cnx
+  //   } catch (err) {
+  //     console.log("conn error : "+err)
+  //     return false// 'Conectar ao Servidor: Failled!'
+  //   }
+  // }
+  // openDB().then(async () => {
+  //   try {
+  //     const query = `select Guid, Number, Code as Name from TablesPlaces where HostGuid = "639D62AF-A73C-4DDD-8EAE-8F2155093C5B"`
 
-      const result = await MSSQL.executeQuery(query);
-      alert(JSON.stringify(result))
-      console.log(result)
-      console.log('show readAll')
-      await MSSQL.close()
-    } catch (erro) {
-      console.log(erro)
-      console.log("Errorrr  :  "+erro)
-      await MSSQL.close()
-    }
-  })
+  //     const result = await MSSQL.executeQuery(query);
+  //     alert(JSON.stringify(result))
+  //     console.log(result)
+  //     console.log('show readAll')
+  //     await MSSQL.close()
+  //   } catch (erro) {
+  //     console.log(erro)
+  //     console.log("Errorrr  :  "+erro)
+  //     await MSSQL.close()
+  //   }
+  // })
   return (
     // <View style={styles.container}>
     <StackNavigation />
